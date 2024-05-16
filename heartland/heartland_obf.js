24f73410d4b87fb8e14481028d225293b1e328fb
wischrendel <chris@chrisjwendel.com>
2024-05-16 10:56:05 -0500

    1.2 Add Heartland Agency

---
+++ heartland/heartland_obf.js
@@ -0,0 +1,131 @@
+var _0x2b23b6 = _0x5157;
+(function (_0x15ec0b, _0x37c234) {
+	var _0x505b80 = _0x5157,
+		_0x1e3740 = _0x15ec0b();
+	while (!![]) {
+		try {
+			var _0x4f5753 =
+				parseInt(_0x505b80(0x15c)) / 0x1 +
+				(parseInt(_0x505b80(0x177)) / 0x2) *
+					(-parseInt(_0x505b80(0x159)) / 0x3) +
+				-parseInt(_0x505b80(0x16b)) / 0x4 +
+				parseInt(_0x505b80(0x157)) / 0x5 +
+				-parseInt(_0x505b80(0x153)) / 0x6 +
+				(-parseInt(_0x505b80(0x167)) / 0x7) *
+					(parseInt(_0x505b80(0x151)) / 0x8) +
+				parseInt(_0x505b80(0x152)) / 0x9;
+			if (_0x4f5753 === _0x37c234) break;
+			else _0x1e3740["push"](_0x1e3740["shift"]());
+		} catch (_0x45da72) {
+			_0x1e3740["push"](_0x1e3740["shift"]());
+		}
+	}
+})(_0x607d, 0x1e011),
+	document[_0x2b23b6(0x174)](_0x2b23b6(0x158), function () {
+		var _0x17c8e7 = _0x2b23b6,
+			_0x302276 = document["createElement"]("div"),
+			_0x52aa5d = window["document"][_0x17c8e7(0x169)][0x0];
+		(_0x302276["innerHTML"] =
+			_0x17c8e7(0x160) + new Date()[_0x17c8e7(0x156)]() + _0x17c8e7(0x162)),
+			document[_0x17c8e7(0x172)][_0x17c8e7(0x166)](_0x302276);
+		var _0xd0b084 = document[_0x17c8e7(0x171)](_0x17c8e7(0x175));
+		(_0xd0b084[_0x17c8e7(0x15f)] = _0x17c8e7(0x154)),
+			document[_0x17c8e7(0x163)][_0x17c8e7(0x166)](_0xd0b084);
+		const _0x4544dd = {
+				text: _0x17c8e7(0x165),
+				background: _0x17c8e7(0x15a),
+				accent: "#707070",
+				unboundText: "#212721",
+				unboundBackground: _0x17c8e7(0x164),
+			},
+			_0x3295c4 = {
+				text: "#d6d1ca",
+				background: _0x17c8e7(0x165),
+				accent: _0x17c8e7(0x164),
+				unboundText: _0x17c8e7(0x165),
+				unboundBackground: _0x17c8e7(0x164),
+			};
+		function _0x3c5159(_0x17c02f) {
+			var _0x9c1cee = _0x17c8e7;
+			const _0x3f9d19 = _0x17c02f === _0x9c1cee(0x161) ? _0x3295c4 : _0x4544dd;
+			console[_0x9c1cee(0x170)](
+				_0x9c1cee(0x15b),
+				"color:\x20" + _0x3f9d19[_0x9c1cee(0x16f)] + _0x9c1cee(0x168),
+				_0x9c1cee(0x173) +
+					_0x3f9d19[_0x9c1cee(0x16a)] +
+					_0x9c1cee(0x155) +
+					_0x3f9d19[_0x9c1cee(0x15e)] +
+					_0x9c1cee(0x16c),
+				_0x9c1cee(0x173) + _0x3f9d19[_0x9c1cee(0x16e)] + _0x9c1cee(0x168)
+			);
+		}
+		window[_0x17c8e7(0x15d)] &&
+		window["matchMedia"]("(prefers-color-scheme:\x20dark)")["matches"]
+			? _0x3c5159(_0x17c8e7(0x161))
+			: _0x3c5159(_0x17c8e7(0x176)),
+			window[_0x17c8e7(0x15d)](_0x17c8e7(0x16d))[_0x17c8e7(0x178)](
+				(_0x29d2bb) => {
+					var _0x3829b1 = _0x17c8e7;
+					_0x3c5159(_0x29d2bb["matches"] ? _0x3829b1(0x161) : _0x3829b1(0x176));
+				}
+			);
+	});
+function _0x5157(_0x383e6b, _0xc5ba00) {
+	var _0x607d82 = _0x607d();
+	return (
+		(_0x5157 = function (_0x5157ab, _0x17ff0d) {
+			_0x5157ab = _0x5157ab - 0x151;
+			var _0x3f2b5d = _0x607d82[_0x5157ab];
+			return _0x3f2b5d;
+		}),
+		_0x5157(_0x383e6b, _0xc5ba00)
+	);
+}
+function _0x607d() {
+	var _0x582be1 = [
+		"addEventListener",
+		"style",
+		"light",
+		"16mrIWlH",
+		"addListener",
+		"37848UNCXjV",
+		"1344465SOhMFE",
+		"1021602jHyRzn",
+		"body\x20{font-size:\x200.8333333333333334vw;}",
+		";\x20background:",
+		"getFullYear",
+		"480575BQwUIz",
+		"DOMContentLoaded",
+		"22071zHNRWb",
+		"#d6d1ca",
+		"%cSite\x20Designed\x20and\x20Built\x20by\x20%cUnbound%c-\x20https://www.unboundcollective.com",
+		"164567ONLkHV",
+		"matchMedia",
+		"unboundBackground",
+		"innerHTML",
+		"\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22copyright\x20w-clearfix\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22https://www.unboundcollective.com?utm_source=heartlandinsuranceagency.com&utm_medium=footer&utm__campaign=general\x22\x20target=\x22_blank\x22\x20class=\x22copyright__bf\x22>Built\x20by:\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<!--\x20Replace\x20below\x20with\x20your\x20actual\x20SVG\x20code\x20-->\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<svg\x20height=\x221em\x22\x20style=\x22vertical-align:\x20middle;\x20padding-bottom:\x200.2em;\x22\x20version=\x221.1\x22\x20id=\x22Layer_1\x22\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20xmlns:xlink=\x22http://www.w3.org/1999/xlink\x22\x20x=\x220px\x22\x20y=\x220px\x22\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20viewBox=\x220\x200\x20631\x20148.83\x22\x20style=\x22enable-background:new\x200\x200\x20631\x20148.83;\x22\x20xml:space=\x22preserve\x22>\x0a\x20\x20\x20\x20<style\x20type=\x22text/css\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20.st0{fill:#FFFFFF;}\x0a\x20\x20\x20\x20</style>\x0a\x20\x20\x20\x20<g\x20id=\x22Group_251\x22\x20transform=\x22translate(-589.086\x20-574.922)\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_1\x22\x20class=\x22st0\x22\x20d=\x22M827.69,627.19c-18.79,0-24.24,15.74-24.24,15.74v-64.06l-28.14,5.89v5\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c6.85,0,10.41,2.66,10.41,10.62v118.77h7.18l5.18-13.08c0,0,8.78,14.59,29.94,14.59c22.8,0,35.44-23.63,35.44-48.7\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20C863.46,645.7,849.57,627.19,827.69,627.19z\x20M823.85,714.26c-20.39,0-20.39-21.81-20.39-21.81V662.2c0,0,0-28.12,20.23-28.12\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c20.25,0,20.25,28.37,20.25,37.71C843.95,681.14,843.95,714.26,823.85,714.26L823.85,714.26z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_2\x22\x20class=\x22st0\x22\x20d=\x22M697.44,584.4h-34.35v5.37c8.74,0,13.28,2.66,13.28,10.62v75.67c0,22.3-8.3,35.75-25.87,35.75\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c-20.36,0-27.16-12.32-27.16-35.75v-75.67c0-10.62,3.77-10.62,11.03-10.62v-5.37H591.5v5.37c7.26,0,11.03,0,11.03,10.62v75.67\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c0,25.6,10.99,44.6,39.74,44.6c30.42,0,41.89-19,41.89-44.6v-75.67c0-7.95,4.54-10.62,13.28-10.62V584.4z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_3\x22\x20class=\x22st0\x22\x20d=\x22M770.96,701.66v-49.47c0-15.81-6.99-24.99-20.19-24.99c-11.84,0-19.71,4.89-32.2,17.38V629.8\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20h-28.14v5.37c6.85,0,10.41,2.66,10.41,10.62v55.87c0,7.95-3.56,10.62-10.41,10.62v5.37h38.56v-5.37c-6.85,0-10.41-2.66-10.41-10.62\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20V650.7c7.35-6.56,15.59-12.9,23.2-12.9c8.07,0,11.45,3.78,11.45,14.65v49.21c0,7.95-3.56,10.62-10.41,10.62v5.37h38.56v-5.37\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20C774.52,712.27,770.96,709.61,770.96,701.66z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_4\x22\x20class=\x22st0\x22\x20d=\x22M1122.55,701.66v-49.47c0-15.81-6.99-24.99-20.19-24.99c-11.84,0-19.71,4.89-32.2,17.38V629.8\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20h-28.14v5.37c6.85,0,10.41,2.66,10.41,10.62v55.87c0,7.95-3.56,10.62-10.41,10.62c-6.85,0-10.41-2.66-10.41-10.62V629.8h-28.14\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20v5.37c6.85,0,10.41,2.66,10.41,10.62v50.96c-7.35,6.56-15.59,12.9-23.2,12.9c-8.07,0-11.45-3.78-11.45-14.65v-65.2h-28.14v5.37\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c6.85,0,10.41,2.66,10.41,10.62v49.47c0,15.81,6.99,24.99,20.19,24.99c11.84,0,19.71-4.89,32.2-17.38v14.78h66.7v-5.37\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c-6.85,0-10.41-2.66-10.41-10.62V650.7c7.35-6.56,15.59-12.9,23.2-12.9c8.07,0,11.45,3.78,11.45,14.65v49.21\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c0,7.95-3.56,10.62-10.41,10.62v5.37h38.56v-5.37C1126.11,712.27,1122.55,709.61,1122.55,701.66z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_5\x22\x20class=\x22st0\x22\x20d=\x22M911.69,627.2c-23.85,0-43.22,20.92-43.22,46.7c0,25.83,19.38,46.76,43.22,46.76\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c23.9,0,43.22-20.92,43.22-46.76C954.91,648.12,935.59,627.2,911.69,627.2z\x20M911.68,713.59c-13.3,0-24.09-17.77-24.09-39.69\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c0-21.86,10.79-39.64,24.09-39.64c13.36,0,24.15,17.78,24.15,39.64C935.84,695.82,925.05,713.59,911.68,713.59L911.68,713.59z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<path\x20id=\x22Path_6\x22\x20class=\x22st0\x22\x20d=\x22M1193.56,717.64h23.88v-5.37c-6.85,0-10.41-2.66-10.41-10.62l-0.01-122.78l-28.14,5.89v5\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c6.67,0,10.22,2.53,10.4,10v35.47c0,0-6.82-8.05-23.08-8.05c-18.12,0-36.89,10.32-36.89,44.76c0,34.53,18.77,48.7,36.56,48.7\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c18.18,0,23.19-15.38,23.19-15.38L1193.56,717.64z\x20M1189.07,693.49c0,3.65-5.68,20.76-19.11,20.76\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20c-20.83,0-21.15-33.12-21.15-42.47s1.8-37.71,21.64-37.71c14.93,0,18.69,16.77,18.69,25.08L1189.07,693.49z\x22/>\x0a\x20\x20\x20\x20</g>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</svg>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22#\x22\x20class=\x22copyright__mt\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20©\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22copyright__mt\x20w-embed\x20w-script\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22#\x22\x20class=\x22copyright__mt\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+		"dark",
+		"\x20Heartland\x20Insurance\x20Agency\x0a\x20\x20\x20\x20\x20\x20\x20\x20</a>\x0a\x20\x20\x20\x20\x20\x20</div>",
+		"head",
+		"#9baeab",
+		"#212721",
+		"appendChild",
+		"77MOwpcs",
+		";\x20font-size:\x2016px;",
+		"styleSheets",
+		"unboundText",
+		"24024NtnpRt",
+		";\x20font-size:\x2016px;\x20font-weight:\x20bold;\x20padding:\x202px\x205px;\x20border-radius:\x204px;",
+		"(prefers-color-scheme:\x20dark)",
+		"accent",
+		"text",
+		"log",
+		"createElement",
+		"body",
+		"color:\x20",
+	];
+	_0x607d = function () {
+		return _0x582be1;
+	};
+	return _0x607d();
+}
